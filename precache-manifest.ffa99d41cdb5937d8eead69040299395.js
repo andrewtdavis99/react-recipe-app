0d6d40590ec96ceb3341024545373f8ab0b5e8dd
Andrew Davis <andrewtdavis@hotmail.co.uk>
2020-02-09 01:06:33 +0000

    Updates

---
+++ precache-manifest.ffa99d41cdb5937d8eead69040299395.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6d96a8cceb5374f689a6832e28ff37",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "03c70d80cf91408cbd69",
+    "url": "/react-recipe-app/static/css/main.74c96865.chunk.css"
+  },
+  {
+    "revision": "41e283160f0089d526cb",
+    "url": "/react-recipe-app/static/js/2.2ad4f29b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-recipe-app/static/js/2.2ad4f29b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c70d80cf91408cbd69",
+    "url": "/react-recipe-app/static/js/main.c878f8b5.chunk.js"
+  },
+  {
+    "revision": "8220a2486d1274b3c6c9",
+    "url": "/react-recipe-app/static/js/runtime-main.dbc2b826.js"
+  }
+]);

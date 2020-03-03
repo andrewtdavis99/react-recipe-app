d247112127a6467c028aba148fa648935bc78901
Andrew Davis <andrewtdavis@hotmail.co.uk>
2020-03-03 14:57:59 +0000

    update resize

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ const App = () => {
       {recipes.map(recipe => (
         <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} 
         image={recipe.recipe.image} ingredients={recipe.recipe.ingredients}/>
-      ))};
+      ))}
       </div>
     </div>
   );
